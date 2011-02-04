40d0fc0e50c23837ecdaf60fa383f11d05993d30
Matt Campbell <woogley@gmail.com>
2011-02-04 12:36:58 -0500

    Setting up index.html as default page

--- main.js
+++ main.js
@@ -10,16 +10,14 @@
 	};
 
 	Blog.prototype.processRequest = function (request, client, input, output) {
+		request.resource = request.resource === '/' && '/index.html' || request.resource;
+
 		/* Serve static file, if it exists */
 		var file = new File(this.getFilePath('/httpdocs' + request.resource));
 		if (file.exists()) {
 			HTTPServer.serveFile(request, output, file);
 			return;
 		}
-
-		var message = 'Hello, from blog.';
-		this.sendResponseHeaders(200, {'content-type': 'text/html'}, output, message.length);
-		output.print(message);
 	};
 
 	HTTPServer.addApplication(Blog);
