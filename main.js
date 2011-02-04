d2131aac2d37bdc5c0a72bd2c0f9990e4a21bb8e
Matt Campbell <woogley@gmail.com>
2011-02-03 21:37:46 -0500

    Adding static file serving

--- main.js
+++ main.js
@@ -2,10 +2,17 @@
 	var Blog = HTTPApplication.extend();
 
 	Blog.prototype.processRequest = function (request, client, input, output) {
+		/* Serve static file, if it exists */
+		var file = new File(this.getFilePath('/httpdocs' + request.resource));
+		if (file.exists()) {
+			HTTPServer.serveFile(request, output, file);
+			return;
+		}
+
 		var message = 'Hello, from blog.';
 		this.sendResponseHeaders(200, {'content-type': 'text/html'}, output, message.length);
 		output.print(message);
 	};
 
-	HTTPServer.addApplication(LocalServer);
+	HTTPServer.addApplication(Blog);
 }());
