4e8b4fbf71f39885a29af4eb7ea6e355cf6b35c2
Matt Campbell <woogley@gmail.com>
2011-02-17 11:53:21 -0500

    Finishing POSTing of data

--- main.js
+++ main.js
@@ -7,7 +7,6 @@
 
 		if (!Blog.Database) {
 			load(this.path + '/lib/database.js');
-			print(Blog.MySQL.getConnection(this.settings.db.username, this.settings.db.password, this.settings.db.database));
 		}
 
 		if (typeof markdownToHtml === 'undefined') {
@@ -23,6 +22,11 @@
 			request.resource = '/new.html';
 		}
 
+		if (/^\/post\/?$/.test(request.resource)) {
+			this.processPost(request, output);
+			return;
+		}
+
 		/* Serve static file, if it exists */
 		var file = new File(this.getFilePath('/httpdocs' + request.resource));
 		if (file.exists()) {
@@ -31,6 +35,24 @@
 		}
 
 		this.sendResponseHeaders(404, {}, output, 0);
+	};
+
+	Blog.prototype.getDBConnection = function () {
+		return Blog.MySQL.getConnection(this.settings.db.username, this.settings.db.password, this.settings.db.database);
+	};
+
+	Blog.prototype.processPost = function (request, output) {
+		var connection, stmt, data = request.data || {};
+		if (/post/i.test(request.method) && data.title && data.message) {
+			connection = this.getDBConnection();
+			stmt = connection.prepareStatement('INSERT INTO posts (`title`, `markdown`, `html`) VALUES(?, ?, ?)');
+			stmt.setString(1, data.title);
+			stmt.setString(2, data.message);
+			stmt.setString(3, markdownToHtml(data.message));
+			stmt.executeUpdate();
+			stmt.close();
+		}
+		this.sendResponseHeaders(302, {location: '/'}, output, 0);
 	};
 
 	HTTPServer.addApplication(Blog);
